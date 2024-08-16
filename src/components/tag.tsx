33f90af684a2ff78d4d9b30b16f31189a22bcd40
Yume <hei.hajatiana@gmail.com>
2024-08-16 14:53:14 +0300

    feat: taggings

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -1,13 +1,29 @@
-import {Link} from "react-router-dom";
+import {Link, useSearchParams} from "react-router-dom";
 
 export interface TagProps {
   tag: string;
 }
 
 export const Tag: React.FC<TagProps> = ({tag}) => {
+  const [p, setSearchParams] = useSearchParams();
+
+  const onTagClick = () => {
+    const entries = Object.fromEntries(p.entries());
+    const previousTags = (entries["tags"] ?? "").split(",");
+    const tags = new Set(previousTags.filter((tag) => !!tag));
+    tags.add(tag);
+
+    const newTags = Array.from(tags).join(",");
+    setSearchParams(new URLSearchParams({...entries, tags: newTags}));
+  };
+
   return (
-    <Link className="text-green-700 hover:underline" to={`?tag=${tag}`}>
+    <div
+      className="text-green-700 hover:underline"
+      role="button"
+      onClick={onTagClick}
+    >
       #{tag}
-    </Link>
+    </div>
   );
 };
